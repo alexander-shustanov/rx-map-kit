e36544141a14f1b7c0d2e13d17ad5f6ad92218cc
dmitriy.kandalin <dmitriykandalin@gmail.com>
2017-04-28 14:32:48 +0400

    enabled overlay property

--- rxmap/src/main/java/com/depthguru/rxmap/overlay/Overlay.java
+++ rxmap/src/main/java/com/depthguru/rxmap/overlay/Overlay.java
@@ -2,9 +2,11 @@ package com.depthguru.rxmap.overlay;
 
 import com.depthguru.rxmap.Projection;
 import com.depthguru.rxmap.rx.SingleItemBuffer;
+import com.depthguru.rxmap.rx.ValveOperator;
 
 import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
+import rx.subjects.BehaviorSubject;
 
 import static rx.Observable.concat;
 import static rx.Observable.just;
@@ -18,18 +20,27 @@ public abstract class Overlay<D> {
 
     private final OverlayDataProvider<D> dataProvider;
 
+    private final BehaviorSubject<Boolean> enabled = BehaviorSubject.create(true);
+
     protected Overlay(OverlayDataProvider<D> dataProvider) {
         this.dataProvider = dataProvider;
     }
 
     final Observable<Drawer> createDrawer(Observable<Projection> projectionObservable) {
         Observable<Drawer> emptyDrawer = just(Drawer.EMPTY_DRAWER);
+
         Observable<Drawer> realDrawer = projectionObservable
-                .filter(projection -> projection.getDiscreteZoom() >= getMinZoom() && projection.getDiscreteZoom() <= getMaxZoom())
-                .compose(this::setupProjectionSubscribe)
-                .compose(dataProvider::fetch)
-                .compose(this::postProcessData)
-                .map(this::createDrawer);
+                .lift(new ValveOperator<>(enabled,
+                        projectionObservable_ ->
+                                projectionObservable_.filter(projection -> projection.getDiscreteZoom() >= getMinZoom() && projection.getDiscreteZoom() <= getMaxZoom())
+                                        .compose(this::setupProjectionSubscribe)
+                                        .compose(dataProvider::fetch)
+                                        .compose(this::postProcessData)
+                                        .map(this::createDrawer),
+                        projectionObservable_ -> projectionObservable_.first().map(projection -> Drawer.EMPTY_DRAWER),
+                        true
+                ));
+
         return concat(emptyDrawer, realDrawer).compose(SingleItemBuffer.dropOldest()).observeOn(AndroidSchedulers.mainThread());
     }
 
@@ -55,4 +66,11 @@ public abstract class Overlay<D> {
         return 23;
     }
 
+    public boolean isEnabled() {
+        return enabled.getValue();
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled.onNext(enabled);
+    }
 }
---
+++ rxmap/src/main/java/com/depthguru/rxmap/rx/ValveOperator.java
@@ -0,0 +1,84 @@
+package com.depthguru.rxmap.rx;
+
+import rx.Observable;
+import rx.Subscriber;
+import rx.Subscription;
+import rx.subjects.BehaviorSubject;
+import rx.subscriptions.Subscriptions;
+
+/**
+ * Created by kandalin on 28.04.17.
+ */
+
+public class ValveOperator<R, T> implements Observable.Operator<R, T> {
+    private final Observable.Transformer<T, R> then_, else_;
+    private final Observable<Boolean> valveObservable;
+    private final boolean initial;
+
+    public ValveOperator(Observable<Boolean> valveObservable, Observable.Transformer<T, R> then_, Observable.Transformer<T, R> else_, boolean initial) {
+        this.then_ = then_;
+        this.else_ = else_;
+        this.valveObservable = valveObservable;
+        this.initial = initial;
+    }
+
+    @Override
+    public Subscriber<? super T> call(Subscriber<? super R> child) {
+        Subscriber<T> subscriber = new Subscriber<T>() {
+
+            private final Object monitor = new Object();
+            private final BehaviorSubject<T> items = BehaviorSubject.create();
+            private Subscription subscription;
+            private boolean state = initial;
+
+            @Override
+            public void onStart() {
+                super.onStart();
+                add(Subscriptions.create(items::onCompleted));
+                add(valveObservable
+                        .distinctUntilChanged()
+                        .subscribe(newState -> {
+                            synchronized (monitor) {
+                                if (state != newState) {
+                                    state = newState;
+                                    setupBranch();
+                                }
+                            }
+                        }, this::onError, this::unsubscribe));
+
+                setupBranch();
+            }
+
+            private synchronized void setupBranch() {
+                if (subscription != null) {
+                    subscription.unsubscribe();
+                    subscription = null;
+                }
+                if (state) {
+                    subscription = items.compose(then_).subscribe(child::onNext, child::onError, () -> {});
+                } else {
+                    subscription = items.compose(else_).subscribe(child::onNext, child::onError, () -> {});
+                }
+                add(subscription);
+            }
+
+            @Override
+            public void onCompleted() {
+                items.onCompleted();
+                unsubscribe();
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                items.onError(e);
+            }
+
+            @Override
+            public void onNext(T t) {
+                items.onNext(t);
+            }
+        };
+        child.add(subscriber);
+        return subscriber;
+    }
+}
